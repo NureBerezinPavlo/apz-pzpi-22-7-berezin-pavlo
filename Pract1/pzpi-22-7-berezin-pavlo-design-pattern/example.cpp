bee48023bfe3d0b60564aa777e3181a1e04a1361
Pavlo Berezin <160422966+NureBerezinPavlo@users.noreply.github.com>
2025-06-02 16:51:46 +0200

    Add files via upload

---
+++ Pract1/pzpi-22-7-berezin-pavlo-design-pattern/example.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <memory>
+
+// інтерфейс стратегії
+class Strategy {
+public:
+    virtual int execute(int a, int b) const = 0;
+    virtual ~Strategy() = default;
+};
+
+// конкретна стратегія — додавання
+class AddStrategy : public Strategy {
+public:
+    int execute(int a, int b) const override {
+        return a + b;
+    }
+};
+
+// конкретна стратегія — віднімання
+class SubtractStrategy : public Strategy {
+public:
+    int execute(int a, int b) const override {
+        return a - b;
+    }
+};
+
+ // контекст
+class Calculator {
+private:
+    std::unique_ptr<Strategy> strategy;
+
+public:
+    // конструктор з ініціалізацією стратегії
+    Calculator(std::unique_ptr<Strategy> s) : strategy(std::move(s)) {}
+
+    // заміна стратегії під час виконання
+    void setStrategy(std::unique_ptr<Strategy> s) {
+        strategy = std::move(s);
+    }
+
+    int calculate(int a, int b) const {
+        if (strategy) {
+            return strategy->execute(a, b);
+        }
+        else {
+            throw std::runtime_error("Strategy not set.");
+        }
+    }
+};
+
+int main() {
+    // починаємо зі стратегії додавання
+    Calculator calc(std::make_unique<AddStrategy>());
+    std::cout << "5 + 3 = " << calc.calculate(5, 3) << std::endl;
+
+    // переходимо до стратегїі віднісмання
+    calc.setStrategy(std::make_unique<SubtractStrategy>());
+    std::cout << "5 - 3 = " << calc.calculate(5, 3) << std::endl;
+
+    return 0;
+}
